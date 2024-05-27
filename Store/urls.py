a491254d4def431391e9b7665903bf8ec2926f76
Mohammad Fahad <mohammadfahad.se@gmail.com>
2024-05-27 22:40:04 +0500

    cart urls

--- Store/urls.py
+++ Store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from Store.views import products
+from Store.views import products, store
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    
     path('', products, name='products'),
+    path('store/', store, name='store'),    
+
 ]
