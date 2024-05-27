de30d29c396cf4d374fdd64725bc24e4ecb0864c
Mohammad Fahad <mohammadfahad.se@gmail.com>
2024-05-27 22:39:23 +0500

    cart init

--- Store/views/home.py
+++ Store/views/home.py
@@ -5,4 +5,7 @@ from Store.models import Product
 def products(request):
     products = Product.objects.all()
     return render(request, 'store/home.html',{'products': products})
+
+def store(request):
+    return render(request, 'store/cart.html')
     
