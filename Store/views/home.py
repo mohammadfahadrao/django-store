e70a08b9a4e3e7fab7678ceaaa57f08b1444be3b
Mohammad Fahad <mohammadfahad.se@gmail.com>
2024-05-28 17:55:07 +0500

    Login decorator

--- Store/views/home.py
+++ Store/views/home.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from Store.models import Product
+from Store.models import Product, CartItem
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+from Store.services.product import upsert_session_cart
+import pdb
+
 
 def products(request):
     products = Product.objects.all()
     return render(request, 'store/home.html',{'products': products})
 
-def store(request):
+@login_required(login_url='/')
+def cart(request):
+    cart_items = CartItem.objects
     return render(request, 'store/cart.html')
+
+def createAndFetchCart(request, id):
+    
+    cart = upsert_session_cart(request,id)
+    return render(request, 'store/cart.html', {'cart': cart})
+
     
