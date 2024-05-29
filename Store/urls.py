ffb1048a1d29c3d1f4bad485f179d69e3f5f5869
Mohammad Fahad <mohammadfahad.se@gmail.com>
2024-05-29 21:33:26 +0500

    Remove item from cart

--- Store/urls.py
+++ Store/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from Store.views import products,cart, createAndFetchCart
+from Store.views import products,cart, createAndFetchCart, remove_cart_item
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', products, name='products'),
     path('cart/', cart, name='store'),
     path('cart/<int:id>/', createAndFetchCart, name='addToCart'),    
+    path('remove-item-from-cart/<int:id>/', remove_cart_item, name='deleteCartItem')
 
 ]
